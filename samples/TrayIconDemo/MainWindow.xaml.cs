49b4b1e67352144a087629688b9648ac97c48f47
Rafael Mohlin <raffe_mohlin@hotmail.com>
2018-07-23 20:50:46 +0200

    Add initial TrayIconDemo

---
+++ samples/TrayIconDemo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TrayIconDemo
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ samples/TrayIconDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Platform;
+using Avalonia.Logging.Serilog;
+
+namespace TrayIconDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        /// <summary>
+        /// This method is needed for IDE previewer infrastructure
+        /// </summary>
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>().LogToDebug().UsePlatformDetect();
+
+        private static void ConfigureAssetAssembly(AppBuilder builder)
+        {
+            AvaloniaLocator.CurrentMutable
+                .GetService<IAssetLoader>()
+                .SetDefaultAssembly(typeof(App).Assembly);
+        }
+    }
+}
